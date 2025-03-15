a5efab672717a4bd792b856e5e8a54abffa59eb0
Pradip <Pradip@DESKTOP-7D2H6S8>
2025-03-15 18:28:16 +0530

    Added Expense Controller.

---
+++ TrackMyExpenses/Controllers/ExpenseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrackMyExpenses.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        public IActionResult AddExpense()
+        {
+            return View();
+        }
+    }
+}
